be2654084e7c0d01e179bb6287cfd060ed6d9d32
Zack P <i-need-fire@hotmail.com>
2019-10-07 21:16:38 -0500

    added restify api and simple server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const restify = require('restify')
+const pkg = require('./package')
+const config = require('./config')
+
+const options = {
+    name: pkg.name,
+    key: config.server.ssl.key || null,
+    certificate: config.server.ssl.certificate || null
+}
+
+const server = restify.createServer(options)
+
+server.get('/', (req, res, next) => {
+    res.send('hello world')
+    next()
+})
+
+server.listen(config.server.port || 8080, () => {
+    console.log(`server online at ${server.url}`)
+})
