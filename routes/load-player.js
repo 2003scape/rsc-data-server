d44f9cf5be43703dcde8f3ffca2751290a861330
Zack P <i-need-fire@hotmail.com>
2019-10-08 11:14:05 -0500

    modified load-player to automatically accept any user/pw

--- routes/load-player.js
+++ routes/load-player.js
@@ -1,8 +1,44 @@
+// TODO: implement maximum login attempts before temporarily banning an ip
+// TODO: find if the player is already logged into another world
+// TODO: remove unused LoginResults
+
+const LoginResult = {
+    success: 0,
+    successReconnect: 1,
+    invalidCredentials: 3,
+    alreadyOnline: 4,
+    clientUpdated: 5,
+    maxConnectionsExceeded: 6,
+    loginAttemptsExceeded: 7,
+    loginServerRejectedSession: 9,
+    temporarilyBanned: 11,
+    permanentlyBanned: 12,
+    membershipRequired: 15,
+    accountLoadingError: 17,
+    stolenAccount: 18,
+    stolenPassword: 22,
+    staff: 25
+}
+
 module.exports = async (server, db) => {
-    server.get('/player/:username', (req, res, next) => {
-        res.json({
-            body: req.params
-        })
-        next()
+    server.get('/player/:username/:password', (req, res) => {
+        try {
+            res.send({
+                result: LoginResult.staff,
+                profile: {
+                    username: req.params.username,
+                    password: req.params.password,
+                    status: 0x4 | 0x2 | 0x1, // admin
+                    x: 122,
+                    y: 657
+                }
+            })
+        } catch (error) {
+            console.error(`error while loading profile: ${error}`)
+
+            res.send({
+                result: LoginResult.accountLoadingError
+            })
+        }
     })
 }
